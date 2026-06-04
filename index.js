7a8e13ec2a352ddfcea52ed682860aac4a844701
yashtrivedi1234 <yashtrivedi.contact@gmail.com>
2026-06-04 21:43:14 +0530

    done

---
+++ index.js
@@ -0,0 +1,185 @@
+// 1. let 2. var 3. const
+// let
+
+// let name = "abc"
+// console.log(name)
+
+// let age = 12
+// console.log(age)
+// age = 13
+// console.log(age)
+
+// {
+//     let nameOfPerson = "XYZ"
+//     console.log(nameOfPerson)
+
+// }
+
+// {
+//   let nameOfPerson = "XYZ";
+// }
+
+// console.log(nameOfPerson);
+
+// const number = 232323;
+// console.log(number)
+
+// {
+//   const number = 232323;
+//   console.log(number);
+// }
+
+// {
+//   const number = 232323;
+// }
+// console.log(number);
+
+// var age = 21
+// console.log(age)
+// var age = 30
+// console.log(age)
+
+// let name = "OP"
+// console.log(name);
+// let name = "OP"
+// console.log(name);
+
+// Data Types
+
+// example 1 number
+// let num = 12;
+// console.log(num);
+// console.log(typeof num);
+
+// example 2 bollean
+// let condition = true;
+// console.log(condition);
+// console.log(typeof condition);
+
+// example string
+// let name = "xyz";
+// console.log(name);
+// console.log(typeof name);
+
+// let results = null
+// console.log(results);
+
+// let arr = [1,2,3,4]
+// console.log(arr)
+// console.log(typeof(arr))
+
+// let obj = { name: "xyz", class: 12 };
+// console.log(obj);
+// console.log(typeof obj);
+// console.log(obj.name);
+
+// arithm operators
+// let a = 5
+// let b = 5
+// console.log(a-b);
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a**b); //exponent mtlb power
+// console.log(a/b);
+// // console.log(a+b);
+
+// comp operator
+// let a = 5
+// let b = 5
+// console.log(a ==b); //lose equlaity oper
+// console.log(a ===b); // strict equlaity oper
+
+// console.log(a!=b);
+// console.log(a!==b);
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+// logical operator
+// let age = 20
+// let drivinglicense = true
+
+// console.log(age>=1 && drivinglicense);
+
+// let isAdmin = false
+// let user = false
+// console.log(isAdmin || user);
+
+// let userLoggedIn = false
+// console.log(!userLoggedIn);
+
+// let userName = "admin"
+// let password = "1234"
+//  if (userName === "admin" && password ==="1234") {
+//     console.log("User LoggedIn Sucessfully");
+//  }
+//  else{
+//     console.log("Invalid crede");
+//  }
+
+// functions
+// type 1 nommal function // function declaration
+// function greet() {
+//   console.log("Hii");
+// }
+// greet();
+
+// function with params
+// function greet(){
+//     console.log("Hii" + name);
+// }
+// greet("XYZ")
+
+// default params
+// function greet(name = "Shivam") {
+//   console.log("Hii" + name);
+// }
+// greet();
+// greet("Rahul");
+
+// function exprssion
+// const mutiply = function (a, b) {
+//   return a * b;
+// };
+// console.log(mutiply(2, 2));
+
+// arrow function
+// const add = (a, b) => {
+//   return a + b;
+// };
+// console.log(add(5, 5));
+
+
+// interview question
+// function test() {
+//   return "HII";
+// }
+// console.log(test());
+
+
+// array
+
+
+// 1. array
+// 2. object
+// strings
+// error handling
+// loops
+// statement
+
+let phones = ["iPhone", "Samsung", "Motorola", "Vivo", "Redmi"]
+// console.log(phones);
+
+// element access
+// console.log(phones[2]);
+
+// array ki length
+// console.log(phones.length);
+
+// add elements 
+// phones.push("Oppo") //end mai add
+// console.log(phones);
+// phones.unshift("Apple") // start mai add
+console.log(phones);
